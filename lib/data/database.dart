425624a5a20fa111004831938732fb97dceaaecc
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-11-03 15:15:32 -0300

    Criando banco de dados

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,22 +1,10 @@
+import 'package:nosso_primeiro_projeto/data/task_dao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 Future<Database> pegandoDatabase() async {
-  const String tabela = "Comando SQL para criação de tabela";
   final String caminhoPego = join(await getDatabasesPath(), "task.db");
   return openDatabase(caminhoPego, onCreate: (db, versao) {
-    db.execute(tabela);
+    db.execute(TaskDao.criandoTabelaTask);
   }, version: 1);
 }
-
-const String talebaTask = """
-CREATE TABLE $_nomeTabela(
-  $_nome TEXT,
-  $_dificuldade INTEGER,
-  $_imagem TEXT,
-)""";
-
-const String _nomeTabela = "tabelaTask";
-const String _nome = "nome";
-const String _dificuldade = "dificuldade";
-const String _imagem = "imagem";

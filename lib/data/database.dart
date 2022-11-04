a7744f7eb7dfbe296724b0bf570fc0e725870a1d
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-11-04 11:41:42 -0300

    atualizando banco de dados

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -3,8 +3,8 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 Future<Database> pegandoDatabase() async {
-  final String caminhoPego = join(await getDatabasesPath(), "task.db");
-  return openDatabase(caminhoPego, onCreate: (db, versao) {
-    db.execute(TaskDao.criandoTabelaTask);
+  final String caminhoPego = join(await getDatabasesPath(), 'task.db');
+  return await openDatabase(caminhoPego, onCreate: (db, versao) {
+    return db.execute(TaskDao.criandoTabelaTask);
   }, version: 1);
 }
